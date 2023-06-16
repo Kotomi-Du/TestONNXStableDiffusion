179c37e40cfe525e9eb7cf387516d632c168bae9
Yaru Du <yaru.du@intel.com>
2023-06-16 16:30:32 +0800

    Change for dml script 1. add static config for text encoder 2. add config for user_defined seed 3. change image name 4. change scheduler to EulerAncestralDiscreteScheduler to get 20 steps, but the result is weird [https://jira.devtools.intel.com/browse/VGD-21828]

--- test_dml.py
+++ test_dml.py
@@ -3,10 +3,15 @@ import os
 import numpy as np
 import argparse
 from diffusers import OnnxStableDiffusionPipeline
+from diffusers.schedulers import DDIMScheduler, LMSDiscreteScheduler, PNDMScheduler, EulerAncestralDiscreteScheduler
 import onnxruntime as ort
 
+import re
+def validateTitle(title):
+    rstr = r"[\/\\\:\*\?\"\<\>\|.]"
+    return re.sub(rstr, r"", title)
 
-def run_inference(model_path, latents_path, prompt, num_images, batch_size, num_inference_steps, static_dims, enable_profiling, enable_intermediate_model):
+def run_inference(model_path, latents_path, prompt, num_images, batch_size, num_inference_steps, static_dims, enable_profiling, enable_intermediate_model,seed_value):
     ort.set_default_logger_severity(3)
 
     print("Loading models into ORT session...")
@@ -25,32 +30,65 @@ def run_inference(model_path, latents_path, prompt, num_images, batch_size, num_
         sess_options.add_free_dimension_override_by_name("unet_sample_channels", 4)
         sess_options.add_free_dimension_override_by_name("unet_sample_height", 64)
         sess_options.add_free_dimension_override_by_name("unet_sample_width", 64)
-        sess_options.add_free_dimension_override_by_name("unet_time_batch", 1)
+        #sess_options.add_free_dimension_override_by_name("unet_time_batch", 1)
         sess_options.add_free_dimension_override_by_name("unet_hidden_batch", batch_size * 2)
         sess_options.add_free_dimension_override_by_name("unet_hidden_sequence", 77)
+
+        # vae_decoder
+        # sess_options.add_free_dimension_override_by_name("decoder_batch", batch_size)
+        # sess_options.add_free_dimension_override_by_name("decoder_channels", 4)
+        # sess_options.add_free_dimension_override_by_name("decoder_height", 64)
+        # sess_options.add_free_dimension_override_by_name("decoder_width", 64)
+
+        #text_encoder
+        sess_options.add_free_dimension_override_by_name("batch", 1)
+        sess_options.add_free_dimension_override_by_name("sequence", 77 )
+        #safe_checker
+        # sess_options.add_free_dimension_override_by_name("sc_batch", 1)
+        # sess_options.add_free_dimension_override_by_name("sc_channels", 3 )
+        # sess_options.add_free_dimension_override_by_name("sc_input_height", 224 )
+        # sess_options.add_free_dimension_override_by_name("sc_input_width", 224 )
+        # sess_options.add_free_dimension_override_by_name("sc_img_width", 512 )
+        # sess_options.add_free_dimension_override_by_name("sc_img_height", 512 )
         
+    
     start_time = time.time()
     pipe = OnnxStableDiffusionPipeline.from_pretrained(model_path, provider="DmlExecutionProvider",sess_options=sess_options)
-    stage1_time = time.time()  
-    
+    scheduler = EulerAncestralDiscreteScheduler.from_pretrained(model_path, subfolder="scheduler") 
+    pipe.scheduler = scheduler
+    stage1_time = time.time()
+
     #generator = np.random.RandomState(2194967295)
-    if not os.path.exists(latents_path):
-        image = pipe(prompt,num_inference_steps = num_inference_steps).images[0]
-    else:
-        latents = np.load(latents_path).astype(np.float16)
-        image = pipe(prompt,num_inference_steps = num_inference_steps,  latents = latents).images[0]
-    stage2_time = time.time()  
     
-    image.save("mansion.png")
+    if not os.path.exists(latents_path):
+        if seed_value is not None:
+            print("tt")
+            generator = np.random.seed(seed=seed_value)
+            image = pipe(prompt,num_inference_steps = num_inference_steps, generator =generator).images[0]
+        else:
+            seed_value='random'
+            image = pipe(prompt,num_inference_steps = num_inference_steps).images[0]
+    else:
+        latents = np.load(latents_path)#.astype(np.float16)
+        image = pipe(prompt,num_inference_steps = num_inference_steps,  latents = latents).images[0]
+    stage2_time = time.time()
+
+    str_image = validateTitle(str(prompt))[:10] + '_seed_' + str(seed_value) + '_step_' + str(num_inference_steps)+'_time_'+str(int(time.time())) + ".png"
+
+    image.save(str_image)
     print("Image generated from {}".format(model_path))
     print("Initialized Time: {:.2f}s".format(stage1_time - start_time))
     print("End to End Inference Time:{:.2f}s:".format(stage2_time - stage1_time))
-    print("Check configuration:\n profiling:{} \n save_optimized_model: {}".format(sess_options.enable_profiling, enable_intermediate_model))
+    print("Check configuration:\n profiling:{} \n save_optimized_model: {} \n onnx optimization level: {}".format(
+        sess_options.enable_profiling, 
+        enable_intermediate_model,
+        sess_options.graph_optimization_level))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_dir", default = r"C:\Users\GAME\Documents\Project\AIGC\Olive\olive0.2_sd1.5", type = str, help="folder of stable diffusion")
     parser.add_argument("--latents_path", default = r"C:\Users\GAME\Documents\Project\AIGC\latents_fp16.npy", type = str, help="file of latents")
+    parser.add_argument("--seed_value", default=None, type=int, help="Number of seed")
     # property
     parser.add_argument("--prompt", default = "a photo of an astronaut riding a horse on mars", type = str, help="prompt")
     parser.add_argument("--num_images", default=1, type=int, help="Number of images to generate")
@@ -75,7 +113,8 @@ if __name__ == "__main__":
                 args.num_inference_steps,
                 use_static_dims,
                 args.enable_profiling,
-                args.enable_intermediate_model
+                args.enable_intermediate_model,
+                args.seed_value
             )
     
     
